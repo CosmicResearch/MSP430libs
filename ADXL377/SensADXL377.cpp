7fe238fa968e923eb3d0a2994d61595a4de53e97
Guillem Castro <guillem.castro@hotmail.com>
2017-12-06 19:45:30 +0100

    Implemented callbacks

--- ADXL377/SensADXL377.cpp
+++ ADXL377/SensADXL377.cpp
@@ -1,13 +1,24 @@
+/*
+    Bondar libraries
+    Copyright (C) 2017  Associació Cosmic Research
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "SensADXL377.h" 
 
 
-#include "Senscape.h"
-#include "SensADXL377.h"
-
-
-
-void ((*SensADXL377::_onStartDone)(error_t));
-void ((*SensADXL377::_onStopDone)(error_t));
-void ((*SensADXL377::_onReadDone)(sensor_data_t *, error_t));
+void ((*SensADXL377::_onStartDone)(error_t)) = NULL;
+void ((*SensADXL377::_onStopDone)(error_t)) = NULL;
+void ((*SensADXL377::_onReadDone)(sensor_data_t *, error_t)) = NULL;
 
 /* Constructors ***************************************************************/
 
@@ -63,6 +74,8 @@ SensADXL377::SensADXL377(uint16_t inchx,
 	SensADXL377::_calib._chanx = 2048;
 	SensADXL377::_calib._chany = 2048;
 	SensADXL377::_calib._chanz = 2048;
+	
+	started = false;
 
 }
 
@@ -93,22 +106,72 @@ boolean  SensADXL377::onSingleDataReadyChannelz(uint16_t data, error_t result) {
 
 /* Public Methods *************************************************************/
 
-error_t SensADXL377::readAccel(){
+error_t SensADXL377::start() {
 
-		// Attach ADC callbacks.
-	SensADXL377::_adcx.attachCallback(onSingleDataReadyChannelx);
-	SensADXL377::_adcy.attachCallback(onSingleDataReadyChannely);
-	SensADXL377::_adcz.attachCallback(onSingleDataReadyChannelz);
+	if (!started) {
+		SensADXL377::_adcx.attachCallback(onSingleDataReadyChannelx);
+		SensADXL377::_adcy.attachCallback(onSingleDataReadyChannely);
+		SensADXL377::_adcz.attachCallback(onSingleDataReadyChannelz);
+		started = true;
+		if (_onStartDone) {
+			_onStartDone(SUCCESS);
+		}
+		return SUCCESS;
+	}
+	else {
+		if (_onStartDone) {
+			_onStartDone(ERROR);
+		}
+		return ERROR;
+	}
 
+}
+
+error_t SensADXL377::stop() {
+
+	if (started) {
+		started = false;
+		if (_onStopDone) {
+			_onStopDone(SUCCESS);
+		}
+		return SUCCESS
+	}
+	else {
+		if (_onStopDone) {
+			_onStopDone(ERROR);
+		}
+		return ERROR;
+	}
+
+}
+
+error_t SensADXL377::read(){
+	if (!started) {
+		return ERROR;
+	}
 		// Start new conversion channel[0..2].
-	SensADXL377::_adcx.read();
-	SensADXL377::_adcy.read();
-	SensADXL377::_adcz.read();
+	if ((error_t ret = SensADXL377::_adcx.read()) != SUCCESS) {
+		return ret;
+	}
+	if ((error_t ret = SensADXL377::_adcy.read()) != SUCCESS) {
+		return ret;
+	}
+	if ((error_t ret = SensADXL377::_adcz.read()) != SUCCESS) {
+		return ret;
+	}
+
+	if (_onReadDone) {
+		adxl377_data_t* ret = new adxl377_data_t;
+		*ret = SensADXL377::_data;
+		_onReadDone(ret, SUCCESS)
+	}
 
 	return SUCCESS;
 }
 
-
+error_t SensADXL377::readNow() {
+	return read();
+}
 
 /* Callbacks ******************************************************************/
 
--- ADXL377/SensADXL377.h
+++ ADXL377/SensADXL377.h
@@ -1,4 +1,17 @@
-
+/*
+    Bondar libraries
+    Copyright (C) 2017  Associació Cosmic Research
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef SENSCAPE_ADXL377_H_
 #define SENSCAPE_ADXL377_H_
@@ -21,13 +34,14 @@ struct adxl377_calib_t {
 };
 
 
-class SensADXL377:SensorClient{
+class SensADXL377 : SensorClient{
 	private:
 		static SensADC _adcx;
 		static SensADC _adcy;
 		static SensADC _adcz;
 		static adxl377_data_t _data;
 		static adxl377_calib_t _calib;
+		boolean_t started;
 
 		static void ((*_onStartDone)(error_t));
 		static void ((*_onStopDone)(error_t));
@@ -49,7 +63,10 @@ class SensADXL377:SensorClient{
 				uint16_t sampcon_ssel,
 				uint16_t sampcon_id);
 
-		error_t readAccel(adxl377_data_t *data);
+		error_t read(void);
+		error_t stop(void);
+		error_t readNow(void);
+		boolean_t isStarted(void);
 
 		/* callbacks */
 		virtual void attachStartDone(void (*)(error_t));
